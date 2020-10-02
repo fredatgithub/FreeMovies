d24dab061759699d9dd122ed6f54af1bd61cb2c8
GeorgCantor <cupsman1986@gmail.com>
2020-10-02 16:06:35 +0300

    added NetworkUtils

--- app/src/main/java/com/georgcantor/freemovies/ui/MainActivity.kt
+++ app/src/main/java/com/georgcantor/freemovies/ui/MainActivity.kt
@@ -2,9 +2,14 @@ package com.georgcantor.freemovies.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isGone
+import androidx.core.view.isVisible
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.georgcantor.freemovies.R
+import com.georgcantor.freemovies.util.NetworkUtils.getNetworkLiveData
+import com.georgcantor.freemovies.util.gone
+import com.georgcantor.freemovies.util.visible
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,5 +19,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         nav_view.setupWithNavController(findNavController(R.id.nav_host_fragment))
+
+        getNetworkLiveData(applicationContext).observe(this) { isConnected ->
+            no_internet_warning.apply {
+                when (isConnected) {
+                    true -> if (isVisible) gone()
+                    false -> if (isGone) visible()
+                }
+            }
+        }
     }
 }
