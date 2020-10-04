cfc7f0ea220641cb9df1dd09e8e27a47ded16e79
GeorgCantor <cupsman1986@gmail.com>
2020-10-04 19:18:40 +0300

    added empty warning

--- app/src/main/java/com/georgcantor/freemovies/ui/fragment/favorites/FavoritesFragment.kt
+++ app/src/main/java/com/georgcantor/freemovies/ui/fragment/favorites/FavoritesFragment.kt
@@ -2,6 +2,8 @@ package com.georgcantor.freemovies.ui.fragment.favorites
 
 import android.os.Bundle
 import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.georgcantor.freemovies.R
@@ -24,13 +26,17 @@ class FavoritesFragment : Fragment(R.layout.fragment_list) {
         super.onViewCreated(view, savedInstanceState)
 
         viewModel.favorites.observe(viewLifecycleOwner) {
+            empty.visibility = if (it.isNullOrEmpty()) VISIBLE else GONE
             recycler_view.setHasFixedSize(true)
             recycler_view.adapter = FavoritesAdapter(it) {
                 (activity as AppCompatActivity).openFragment(DetailsFragment().apply {
                     arguments = Bundle().apply { putParcelable(VIDEO_ITEM, it) }
                 }, false)
             }
+            refresh_layout.isRefreshing = false
         }
+
+        refresh_layout.setOnRefreshListener { viewModel.getFavorites() }
     }
 
     override fun onResume() {
